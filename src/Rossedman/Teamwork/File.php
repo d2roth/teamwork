38185fe618bbd0fce7a30500f3652e8919bd21d3
adamlytics <adam@disruptiveadvertising.com>
2019-11-14 14:55:35 -0700

    Update File.php

--- src/Rossedman/Teamwork/File.php
+++ src/Rossedman/Teamwork/File.php
@@ -1,1 +1,28 @@
+<?php  namespace Rossedman\Teamwork;
 
+use Rossedman\Teamwork\Traits\TimeTrait;
+use Rossedman\Teamwork\Traits\RestfulTrait;
+
+class File extends AbstractObject {
+
+    use RestfulTrait, TimeTrait;
+
+    protected $wrapper  = 'pendingfile';
+
+    protected $endpoint = 'pendingfiles';
+
+    /**
+     * Upload a file
+     * POST /pendingfiles.json
+     *
+     * @param null $args
+     *
+     * @return mixed
+     */
+    public function uploadFile($args = null)
+    {
+        $this->areArgumentsValid($args, ['file']);
+
+        return $this->client->post($this->endpoint, $args)->response();
+    }
+}
